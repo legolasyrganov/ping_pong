600f2e7712eec2b2c92336cb85a1156f1b6602fc
legolasyrganov <102894027+legolasyrganov@users.noreply.github.com>
2022-04-17 09:12:26 +0300

    v2 добавлен класс GameSprite

--- ping.py
+++ ping.py
@@ -1,5 +1,5 @@
 from pygame import *
-init()
+
 
 display.set_caption("Ping_pong")
 window = display.set_mode((700, 500))
@@ -7,6 +7,24 @@ background = transform.scale(image.load("r.png"), (700, 500))
 
 clock = time.Clock()
 
+class GameSprite(sprite.Sprite):
+  # конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # Вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+  # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 game = True
 while game:
